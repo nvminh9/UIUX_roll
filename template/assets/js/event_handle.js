52276bd5732cc3dfca99579f0027f578d682c590
minhngo <minhcorn.2@gmail.com>
2024-05-31 16:55:58 +0700

    cập nhật js của nút di chuyển slide hình bài viết...

--- template/assets/js/event_handle.js
+++ template/assets/js/event_handle.js
@@ -426,5 +426,19 @@ for(let i=0; i<postImage.length; i++){
 // -----------------------------------------------------------------
 
 // --------------------*** BUTTON MOVE SLIDE POST IMAGES ***---------------------
+// tất cả các slide hình của các bài viết đã được load...
+const postImages = document.getElementsByClassName('postImages');
+// nút di chuyển slide hình qua trái
+const btnLeftPostImages = document.getElementsByClassName('btnLeftPostImages');
+// nút di chuyển slide hình qua phải
+const btnRightPostImages = document.getElementsByClassName('btnRightPostImages');
 
+for(let i=0; i<postImages.length; i++){
+  btnLeftPostImages[i].addEventListener('click', function(e){
+    alert("Nút Left của slide thứ " + i);
+  });
+  btnRightPostImages[i].addEventListener('click', function(e){
+    alert("Nút Right của slide thứ " + i);
+  });
+}
 // ------------------------------------------------------------------------------
